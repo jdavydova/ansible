9b0b52475d061ff882f6ba56d4939973c227d88f
Julia Davydova <juliada888@gmail.com>
2026-08-22 13:23:00 +0300

    add ansible exrsice

--- ansible-exercises/src/main/java/com/example/App.java
+++ ansible-exercises/src/main/java/com/example/App.java
@@ -1,33 +1,27 @@
 package com.example;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-public class Application {
+public class App {
 
     public static void main(String[] args)
     {
-        SpringApplication.run(Application.class, args);
-        // add your code snippet here
+        SpringApplication.run(App.class, args);
     }
 
     @PostConstruct
     public void init()
     {
-        Logger log = LoggerFactory.getLogger(Application.class);
+        Logger log = LoggerFactory.getLogger(App.class);
         log.info("Java app started");
     }
 
     public String getStatus() {
         return "OK";
-    }
-
-    public boolean getCondition(boolean condition) {
-        return condition;
     }
 }
